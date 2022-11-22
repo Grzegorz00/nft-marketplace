9be49771f12b1ab6770f0d4f43ab096d94aac76e
s21009 <s21009@pjwstk.edu.pl>
2022-11-22 14:40:55 +0100

    changed serchbars styling

--- nft-frontend/components/Navbar.jsx
+++ nft-frontend/components/Navbar.jsx
@@ -7,6 +7,7 @@ import {
     icon
   } from '@fortawesome/fontawesome-svg-core/import.macro'
 
+
 export default function Navbar(){
     return(
 <nav className="
@@ -29,19 +30,19 @@ export default function Navbar(){
                     Create Nft
                 </Link>
                 <Link 
-                    href="create-nft" 
+                    href="marketplace" 
                     className="text-dark-400 hover:text-pink-500 text-indigo-500">
                     Marketplace
                 </Link>
             </div>
-            <div class="relative flex items-center">
-                <svg className='w-5 h-5 absolute ml-3 pointer-events-none'>
+            <div className="relative flex items-center">
+                <svg className='w-5 h-5 absolute ml-3 pointer-events-auto'>
                     <FontAwesomeIcon icon={solid('magnifying-glass-dollar')} className='text-indigo-500'/>
                 </svg>
                 <input
                     type="text"
                     placeholder="Search"
-                    className="w-full pr-2 pl-10 py-0 placeholder-indigo-300 text-indigo-900 rounded-2xl border-2 border-indigo-500"/>
+                    className="appearance-none border-2 border-indigo-500 rounded-2xl w-full pr-2 pl-10 py-0 text-indigo-900 leading-tight focus:outline-none focus:border-purple-500"/>
             </div>
         </div>
     </nav>
---
+++ nft-frontend/pages/marketplace.jsx
@@ -0,0 +1,7 @@
+export default function Home() {
+    return (
+      <div className="">
+        <h1>CD......</h1>
+      </div>
+    )
+  }
