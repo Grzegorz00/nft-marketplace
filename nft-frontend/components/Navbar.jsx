c05686abf6c6e4566e71f0b943e0619968ad058b
s21009 <s21009@pjwstk.edu.pl>
2022-11-23 19:06:35 +0100

    corect scaled navbar

--- nft-frontend/components/Navbar.jsx
+++ nft-frontend/components/Navbar.jsx
@@ -13,33 +13,35 @@ export default function Navbar(){
     py-4
     bg-gray-100
     shadow-lg
-    navbar navbar-expand-lg navbar-light
+    navbar 
+    navbar-expand-lg 
+    navbar-light
     font-mono
     text-2xl">
-        <div className="container flex flex-wrap justify-between items-center"> 
+        <div className="flex justify-between items-center ml-10 mr-10"> 
 
             {/* LOGO */}
-            <div className='flex items-center'>
-                <svg className='w-14 h-14  ml-5'>
+            <div className='flex items-center space-x-5'>
+                <svg className='w-14 h-14'>
                     <FontAwesomeIcon icon={solid('store')} className='text-indigo-500'/>
                 </svg>
-                <span className="self-center text-4xl text-pink-500">NFT Marketplace</span>
+                <span className="text-4xl text-pink-500">NFT Marketplace</span>
             </div>
 
             {/* LINK NAVBAR*/}
-            <div className="flex justify-between items-cente">
+            <div className="flex justify-between items-center">
                 <ul className='flex items-center space-x-5 px-7'>
                     <li>
                         <Link
                             href="/"
-                            className="text-dark-400 hover:text-pink-500 text-indigo-500">
+                            className="hover:text-pink-500 text-indigo-500">
                             Home
                         </Link>
                     </li>
                     <li>
                         <Link
                             href="create-nft" 
-                            className="text-dark-400 hover:text-pink-500 text-indigo-500">
+                            className="hover:text-pink-500 text-indigo-500">
                             Create Nft
                         </Link>
                     </li>
@@ -64,22 +66,11 @@ export default function Navbar(){
                 </span>
             </div>
 
-            {/* SEARCHBAR */}
-            {/* <span className="flex items-center">
-                <svg className='w-5 h-5 absolute ml-3 pointer-events-auto'>
-                    <FontAwesomeIcon icon={solid('magnifying-glass-dollar')} className='text-indigo-500'/>
-                </svg>
-                <input
-                    type="text"
-                    placeholder="Search"
-                    className="appearance-none border-2 border-indigo-500 rounded-2xl w-full pr-2 pl-10 py-0 text-indigo-900 leading-tight focus:outline-none focus:border-pink-500"/>
-            </span> */}
-
             {/* WALLET */}
-            <div className='position absolute right-16'>
-                <span className='flex items-center'>
-                    <button className='relative flex items-center' onClick={()=>console.log("Metamask")}>
-                        <svg className='w-10 h-10 absolute pointer-events-auto'>
+            <div className=''>
+                <span className='items-center'>
+                    <button className='flex items-center' onClick={()=>console.log("Metamask")}>
+                        <svg className='w-10 h-10 pointer-events-auto'>
                             <FontAwesomeIcon icon={solid('wallet')} className='text-indigo-500 hover:text-pink-500'/>
                         </svg>
                     </button>
