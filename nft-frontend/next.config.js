03186040bf5087faf092270ddd46fe67eed6ed45
Grzegorz00 <g.swiecickii@gmail.com>
2022-11-24 21:51:21 +0100

    added local environments

--- nft-frontend/next.config.js
+++ nft-frontend/next.config.js
@@ -3,6 +3,11 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ["nft-marketplace-gsps.infura-ipfs.io","infura-ipfs.io"]
+  },
+  env: {
+    PROJECT_ID: process.env.PROJECT_ID,
+    PROJECT_SECRET_KEY: process.env.PROJECT_SECRET_KEY,
+    SUBDOMAIN: process.env.SUBDOMAIN
   }
 }
 
