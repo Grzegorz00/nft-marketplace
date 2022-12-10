c89aae06460c237d2ae822a8c973309d56b32e5b
Grzegorz00 <g.swiecickii@gmail.com>
2022-12-10 19:17:21 +0100

    fixed typo

--- nft-frontend/pages/marketplace.jsx
+++ nft-frontend/pages/marketplace.jsx
@@ -53,7 +53,7 @@ export default function Marketplace() {
               <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-4">
                 {
                   nfts.map((nft, i) => (
-                    <div classname="" key={i}>
+                    <div className="" key={i}>
                       <CartNFT nftDetails={nft}/>
                     </div>
                   ))
