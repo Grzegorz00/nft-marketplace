94f31d530d3e278974b05ebfd044da39eeffe883
Grzegorz00 <g.swiecickii@gmail.com>
2023-01-04 23:37:25 +0100

    added default avatar

--- nft-frontend/components/UserAvatarDropzone.jsx
+++ nft-frontend/components/UserAvatarDropzone.jsx
@@ -4,11 +4,13 @@ import { NFTMarketplaceContext } from "../context/NFTMarketplaceContext";
 import { useDropzone } from 'react-dropzone'
 import Image from 'next/image'
 import { ADD_AVATAR } from '../queries/users';
+import avatar from "../styles/images/avatar.BMP"
 
 export default function UserAvatarDropzone ({avatarUrl}) {
   const [addImage] = useMutation(ADD_AVATAR)
   const { uploadToIPFS, currentAccount } = useContext(NFTMarketplaceContext);
   const [imageUrl, setImageUrl] = useState(avatarUrl)
+  
 
   const onDrop = useCallback(async (acceptedFile) => {
       const url = await uploadToIPFS(acceptedFile[0])
@@ -21,11 +23,10 @@ export default function UserAvatarDropzone ({avatarUrl}) {
     <div{...getRootProps()}>
       <input{...getInputProps()} />
       <div className='overflow-hidden outline-none w-72 h-72 rounded-full hover:bg cursor-pointer focus:outline-none border-4 border-white shadow-grey-700/50 shadow-lg'>
-            {imageUrl &&
             <Image 
-                src={imageUrl}
+                src={imageUrl ? imageUrl : avatar}
                 className="object-cover w-full h-72 hover:brightness-50"
-                width={864} height={864} alt='NFT'/> }
+                width={864} height={864} alt='NFT'/>
       </div>
     </div>
   )
