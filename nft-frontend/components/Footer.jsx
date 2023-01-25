ce24d8748149154b4db2fe0a3025055ab1a9636d
PiotrSadolewski <piotr.sadolewski00@gmail.com>
2023-01-25 21:25:49 +0100

    changes

--- nft-frontend/components/Footer.jsx
+++ nft-frontend/components/Footer.jsx
@@ -1,22 +1,22 @@
 export default function Footer(){
     return(
-        <footer className="p-6 bg-gray-100 shadow-inner w-full mt-10 bottom-0 realtive">
+        <footer className="p-6 md:p-3 bg-gray-100 shadow-inner w-full mt-10 md:mt-6 bottom-0 realtive">
             <div className="flex space-x-64">
                 <div className="flex items-center space-x-6">
-                    <div className="text-3xl font-custom text-transparent gradient bg-clip-text">
+                    <div className="text-3xl md:text-xl font-custom text-transparent gradient bg-clip-text">
                         <p>Designed by</p>
                     </div>
-                    <div className="grid text-indigo-900 font-bold">
+                    <div className="grid text-indigo-900 font-bold md:text-sm">
                         <p>Piotr Sadolewski</p>
                         <p>Grzegorz Święcicki</p>
                     </div>
                 </div>
 
                 <div className="flex items-center space-x-6">
-                    <div className="text-3xl font-custom text-transparent gradient bg-clip-text">
+                    <div className="text-3xl md:text-xl font-custom text-transparent gradient bg-clip-text">
                         <p>Contact us</p>
                     </div>
-                    <div className="grid text-indigo-900 font-bold">
+                    <div className="grid text-indigo-900 font-bold md:text-sm">
                         <p>piotr.sadolewski00@gmail.com</p>
                         <p>grzegorz.swiecicki00@gmail.com</p>
                     </div>
