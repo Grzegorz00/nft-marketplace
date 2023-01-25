396ec59b0e6295bfe5203d28cff3eea77b172169
s21009 <s21009@pjwstk.edu.pl>
2023-01-25 16:53:13 +0100

    changed nft

--- nft-frontend/tailwind.config.js
+++ nft-frontend/tailwind.config.js
@@ -8,13 +8,15 @@ module.exports = {
   theme: {
     extend: {
       spacing: {
+        '01' : '0.02rem',
         '115': '30rem',
         '120': '32.25rem',
         '128': '32rem',
         '144': '36rem',
         '160' : '42rem',
-        '500' : '500px',
-        '600' : '600px'
+        '500' : '28.125rem',
+        '600' : '44.125rem',
+        '728' : '43rem'
       },
       fontFamily: {
         custom: "'Audiowide', cursive"
