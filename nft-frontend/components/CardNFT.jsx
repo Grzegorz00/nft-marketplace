480dd042f824f6ea9bdca36867a960241f1c7fe2
s21009 <s21009@pjwstk.edu.pl>
2023-01-26 02:07:29 +0100

    fixed card nft

--- nft-frontend/components/CardNFT.jsx
+++ nft-frontend/components/CardNFT.jsx
@@ -16,14 +16,14 @@ export default function CartNFT({nftDetails, cardSize}){
     }
     
     return(
-        <div className="flex justify-center">
+        <div className={`flex justify-center ${cardSize == 'sm' ? 'w-52 h-360' : 'w-80 h-472'}`}>
             <div className="rounded-xl overflow-hidden group shadow-lg shadow-cyan-500/50">
                 <Link href={typeof(nftDetails.sold) === "undefined" ? "" : { pathname: "/nft", query: nftDetails}}>
                     <Image src={nftDetails.fileUrl}
                         className={`object-cover transition-transform duration-300 group-hover:scale-110 object-top ${cardSize == 'sm' ? 'h-52 w-52' : 'h-80 w-80'}`}
                         width={500} height={500} alt='NFT'/>
                     <div 
-                        className={`p-4 relative bg-white ${cardSize == 'sm' ? 'w-52' : 'w-80'}`}>
+                        className="p-4 relative bg-white">
                         <p className="text-2xl text-indigo-900 font-custom">{stringMaxLengthAdjuster(nftDetails.name,18)}</p>
                         <p className="text-gray-500">{stringMaxLengthAdjuster(nftDetails.description, 32)}</p>
                     </div>
